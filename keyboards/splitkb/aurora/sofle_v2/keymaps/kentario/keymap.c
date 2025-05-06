2c2d8e7f2f6ecc3f4553b6df0a7c010ca7bd38bb
Kentaro Wuertele <ken@wuertele.com>
2025-05-05 22:00:17 -0700

    Added repeat key, changed stuff for ext and in ext to not be one shot so that they don't get left on on accident, and shuffled around a few symbols.

--- keyboards/splitkb/aurora/sofle_v2/keymaps/kentario/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/kentario/keymap.c
@@ -22,6 +22,7 @@ enum layers {
 
 #define OS_SYM OSL(SYM)
 #define OS_EXT OSL(EXT)
+#define MO_EXT MO(EXT)
 #define OS_NUM OSL(NUM)
 
 enum my_keycodes {
@@ -35,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   KC_ESC    , KC_Q      , KC_W      , KC_F      , KC_P      , KC_B      ,                         KC_J      , KC_L      , KC_U      , KC_Y      , KC_SCLN   , KC_BSPC   ,
 		   KC_TAB    , KC_A      , KC_R      , KC_S      , KC_T      , KC_G      ,                         KC_M      , KC_N      , KC_E      , KC_I      , KC_O      , KC_QUOT   ,
 		   _______   , KC_Z      , KC_X      , KC_C      , KC_D      , KC_V      , _______   , _______   , KC_K      , KC_H      , KC_COMM   , KC_DOT    , KC_SLSH   , _______   ,
-		                           _______   , _______   , OS_LSFT   , KC_SPC    , OS_NUM    , OS_SYM    , KC_ENT    , OS_RSFT   , _______   , _______
+		                           KC_LGUI   , QK_REP    , OS_LSFT   , KC_SPC    , OS_NUM    , OS_SYM    , KC_ENT    , OS_RSFT   , QK_AREP   , _______
 
 		   ),
 
@@ -43,22 +44,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     , KC_F6     ,                         KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    ,
 		   KC_GRV    , KC_UNDS   , KC_LCBR   , KC_LPRN   , KC_LBRC   , KC_MINS   ,                         KC_PLUS   , KC_RBRC   , KC_RPRN   , KC_RCBR   , KC_SCLN   , KC_BSPC   ,
 		   KC_TILD   , KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                         KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , KC_BSLS   ,
-		   _______   , KC_EXLM   , KC_AT     , KC_PLUS   , KC_DLR    , KC_PERC   , _______   , _______   , KC_CIRC   , KC_AMPR   , KC_ASTR   , KC_EQL    , KC_RPRN   , KC_PIPE   ,
-		                           _______   , _______   , _______   , _______   , _______   , OS_EXT    , _______   , _______   , _______   , _______
+		   _______   , KC_EXLM   , KC_AT     , KC_PLUS   , KC_DLR    , KC_PERC   , _______   , _______   , KC_CIRC   , KC_AMPR   , KC_ASTR   , KC_DOT    , KC_SLSH   , KC_PIPE   ,
+		                           _______   , _______   , _______   , _______   , _______   , MO_EXT    , _______   , _______   , _______   , _______
  		 ),
 
     [SYM] = LAYOUT(
 		   KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     , KC_F6     ,                         KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    ,
-		   KC_GRV    , KC_UNDS   , KC_LCBR   , KC_LPRN   , KC_LBRC   , KC_MINS   ,                         KC_PLUS   , KC_RBRC   , KC_RPRN   , KC_RCBR   , KC_SCLN   , KC_COLN   ,
+		   KC_GRV    , KC_UNDS   , KC_LCBR   , KC_LPRN   , KC_LBRC   , KC_MINS   ,                         KC_PLUS   , KC_RBRC   , KC_RPRN   , KC_RCBR   , KC_SCLN   , KC_BSPC   ,
 		   KC_GRV    , KC_UNDS   , KC_EQUAL  , KC_PLUS   , KC_MINUS  , KC_LABK   ,                         KC_RABK   , KC_ASTR   , KC_8      , KC_9      , KC_PIPE   , KC_BSLS   ,
 		   _______   , KC_EXLM   , KC_AT     , KC_PLUS   , KC_DLR    , KC_PERC   , _______   , _______   , KC_CIRC   , KC_AMPR   , KC_ASTR   , KC_EQL    , KC_RPRN   , KC_PIPE   ,
-		                           _______   , _______   , _______   , _______   , OS_EXT    , _______   , _______   , _______   , _______   , _______
+		                           _______   , _______   , _______   , _______   , MO_EXT    , _______   , _______   , _______   , _______   , _______
 		   ),
 
     [EXT] = LAYOUT(
 		   _______   , _______   , _______   , _______   , _______   , _______   ,                         _______   , _______   , _______   , _______   , _______   , _______   ,
 		   _______   , KC_INS    , KC_PSCR   , KC_APP    , XXXXXXX   , XXXXXXX   ,                         KC_PGUP   , C(KC_LEFT), KC_UP     , C(KC_RGHT), C(KC_BSPC), KC_BSPC   ,
-		   _______   , KC_LGUI   , OS_LALT   , OS_LCTL   , OS_LSFT   , KC_CAPS   ,                         KC_PGDN   , OS_LSFT   , OS_LCTL   , OS_RALT   , KC_RGUI   , KC_DEL    ,
+		   _______   , KC_LGUI   , KC_LALT   , KC_LCTL   , KC_LSFT   , KC_CAPS   ,                         KC_PGDN   , KC_LEFT   , KC_DOWN   , KC_RIGHT  , KC_RGUI   , KC_DEL    ,
 		   _______   , C(KC_Z)   , C(KC_X)   , C(KC_C)   , C(KC_V)   , XXXXXXX   , _______   , _______   , XXXXXXX   , KC_HOME   , XXXXXXX   , KC_END    , XXXXXXX   , _______   ,
 		                           _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______
 		   )
