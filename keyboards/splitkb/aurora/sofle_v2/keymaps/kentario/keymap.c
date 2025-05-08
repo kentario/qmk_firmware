3b19e6bee7e25c8ae6b194d44c293dcebe9d1256
Kentaro Wuertele <ken@wuertele.com>
2025-05-07 20:23:08 -0700

    Moved function layer from symbols to ext because they aren't symbols. Also did some more slight shuffling of keys.

--- keyboards/splitkb/aurora/sofle_v2/keymaps/kentario/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/kentario/keymap.c
@@ -1,4 +1,3 @@
-// TODO: Maybe make EXT + SPC do ctrl or something like that.
 #include QMK_KEYBOARD_H
 #if __has_include("keymap.h")
 #    include "keymap.h"
@@ -35,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   _______   , KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                         KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , CLEAR_OS  ,
 		   KC_ESC    , KC_Q      , KC_W      , KC_F      , KC_P      , KC_B      ,                         KC_J      , KC_L      , KC_U      , KC_Y      , KC_SCLN   , KC_BSPC   ,
 		   KC_TAB    , KC_A      , KC_R      , KC_S      , KC_T      , KC_G      ,                         KC_M      , KC_N      , KC_E      , KC_I      , KC_O      , KC_QUOT   ,
-		   _______   , KC_Z      , KC_X      , KC_C      , KC_D      , KC_V      , _______   , _______   , KC_K      , KC_H      , KC_COMM   , KC_DOT    , KC_SLSH   , _______   ,
+		   KC_LGUI   , KC_Z      , KC_X      , KC_C      , KC_D      , KC_V      , _______   , _______   , KC_K      , KC_H      , KC_COMM   , KC_DOT    , KC_SLSH   , _______   ,
 		                           KC_LGUI   , QK_REP    , OS_LSFT   , KC_SPC    , OS_NUM    , OS_SYM    , KC_ENT    , OS_RSFT   , QK_AREP   , _______
 
 		   ),
@@ -49,16 +48,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  		 ),
 
     [SYM] = LAYOUT(
-		   KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     , KC_F6     ,                         KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    ,
+		   _______   , _______   , _______   , _______   , _______   , _______   ,                         _______   , _______   , _______   , _______   , _______   , _______   ,
 		   KC_GRV    , KC_UNDS   , KC_LCBR   , KC_LPRN   , KC_LBRC   , KC_MINS   ,                         KC_PLUS   , KC_RBRC   , KC_RPRN   , KC_RCBR   , KC_SCLN   , KC_BSPC   ,
-		   KC_GRV    , KC_UNDS   , KC_EQUAL  , KC_PLUS   , KC_MINUS  , KC_LABK   ,                         KC_RABK   , KC_ASTR   , KC_8      , KC_9      , KC_PIPE   , KC_BSLS   ,
+		   KC_TAB    , KC_UNDS   , KC_EQUAL  , KC_PLUS   , KC_MINUS  , KC_LABK   ,                         KC_RABK   , KC_ASTR   , KC_8      , KC_9      , KC_PIPE   , KC_BSLS   ,
 		   _______   , KC_EXLM   , KC_AT     , KC_PLUS   , KC_DLR    , KC_PERC   , _______   , _______   , KC_CIRC   , KC_AMPR   , KC_ASTR   , KC_EQL    , KC_RPRN   , KC_PIPE   ,
 		                           _______   , _______   , _______   , _______   , MO_EXT    , _______   , _______   , _______   , _______   , _______
 		   ),
 
     [EXT] = LAYOUT(
-		   _______   , _______   , _______   , _______   , _______   , _______   ,                         _______   , _______   , _______   , _______   , _______   , _______   ,
-		   _______   , KC_INS    , KC_PSCR   , KC_APP    , XXXXXXX   , XXXXXXX   ,                         KC_PGUP   , C(KC_LEFT), KC_UP     , C(KC_RGHT), C(KC_BSPC), KC_BSPC   ,
+		   KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     , KC_F6     ,                         KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    ,
+		   _______   , KC_INS    , KC_PSCR   , KC_APP    , XXXXXXX   , XXXXXXX   ,                         KC_PGUP   , C(KC_LEFT), KC_UP     , C(KC_RGHT), C(KC_BSPC), KC_DEL   ,
 		   _______   , KC_LGUI   , KC_LALT   , KC_LCTL   , KC_LSFT   , KC_CAPS   ,                         KC_PGDN   , KC_LEFT   , KC_DOWN   , KC_RIGHT  , KC_RGUI   , KC_DEL    ,
 		   _______   , C(KC_Z)   , C(KC_X)   , C(KC_C)   , C(KC_V)   , XXXXXXX   , _______   , _______   , XXXXXXX   , KC_HOME   , XXXXXXX   , KC_END    , XXXXXXX   , _______   ,
 		                           _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______
