933626d0102a215b38daed82eae3084c48140d80
Kentaro Wuertele <ken@wuertele.com>
2025-05-11 21:00:35 -0700

    Some more slight shuffling of gui and other stuff

--- keyboards/splitkb/aurora/sofle_v2/keymaps/kentario/keymap.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/kentario/keymap.c
@@ -34,8 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   _______   , KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                         KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , CLEAR_OS  ,
 		   KC_ESC    , KC_Q      , KC_W      , KC_F      , KC_P      , KC_B      ,                         KC_J      , KC_L      , KC_U      , KC_Y      , KC_SCLN   , KC_BSPC   ,
 		   KC_TAB    , KC_A      , KC_R      , KC_S      , KC_T      , KC_G      ,                         KC_M      , KC_N      , KC_E      , KC_I      , KC_O      , KC_QUOT   ,
-		   KC_LGUI   , KC_Z      , KC_X      , KC_C      , KC_D      , KC_V      , _______   , _______   , KC_K      , KC_H      , KC_COMM   , KC_DOT    , KC_SLSH   , _______   ,
-		                           KC_LGUI   , QK_REP    , OS_LSFT   , KC_SPC    , OS_NUM    , OS_SYM    , KC_ENT    , OS_RSFT   , QK_AREP   , _______
+		   KC_LGUI   , KC_Z      , KC_X      , KC_C      , KC_D      , KC_V      , _______   , _______   , KC_K      , KC_H      , KC_COMM   , KC_DOT    , KC_SLSH   , OS_RGUI   ,
+		                           _______   , QK_REP    , OS_LSFT   , KC_SPC    , OS_NUM    , OS_SYM    , KC_ENT    , OS_RSFT   , QK_AREP   , _______
 
 		   ),
 
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		   KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     , KC_F6     ,                         KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    ,
 		   KC_GRV    , KC_UNDS   , KC_LCBR   , KC_LPRN   , KC_LBRC   , KC_MINS   ,                         KC_PLUS   , KC_RBRC   , KC_RPRN   , KC_RCBR   , KC_SCLN   , KC_BSPC   ,
 		   KC_TILD   , KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,                         KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , KC_BSLS   ,
-		   _______   , KC_EXLM   , KC_AT     , KC_PLUS   , KC_DLR    , KC_PERC   , _______   , _______   , KC_CIRC   , KC_AMPR   , KC_ASTR   , KC_DOT    , KC_SLSH   , KC_PIPE   ,
+		   _______   , KC_EXLM   , KC_AT     , KC_HASH   , KC_DLR    , KC_PERC   , _______   , _______   , KC_CIRC   , KC_AMPR   , KC_ASTR   , KC_DOT    , KC_SLSH   , KC_PIPE   ,
 		                           _______   , _______   , _______   , _______   , _______   , MO_EXT    , _______   , _______   , _______   , _______
  		 ),
 
